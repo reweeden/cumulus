78ccc1bfc2402a9bec70d2b60c9f18a7c84bffbd
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-09-10 18:42:32 -0600

    Update jasmine tests to retry on failure, but still report/provide logs

--- example/scripts/run_test.sh
+++ example/scripts/run_test.sh
@@ -16,8 +16,15 @@ if [ "$result" -eq "0" ]; then
   echo "$TIMESTAMP ../node_modules/.bin/jasmine $2 PASSED"
   mv "$outputPath" "$1/${specName}-passed.txt"
 else
-  echo "$TIMESTAMP ../node_modules/.bin/jasmine $2 FAILED"
   mv "$outputPath" "$1/${specName}-failed.txt"
+  echo "$TIMESTAMP - FAILED -- Retrying ${specName}"
+  ../node_modules/.bin/jasmine --reporter=defaultReporter --no-color "$2" > "$outputPath" 2>&1
+  result=$?
+  if [ "$result" -eq "0" ]; then
+    echo "$TIMESTAMP ../node_modules/.bin/jasmine $2 PASSED ON RETRY"
+  else
+    echo "$TIMESTAMP ../node_modules/.bin/jasmine $2 FAILED"
+  fi
 fi
 
 exit $result
